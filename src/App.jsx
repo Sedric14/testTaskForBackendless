6b5d9c85f374cab2fda0a4502961e66102239373
Sedric <sedricgreen6@gmail.com>
2023-10-26 22:25:11 +0300

    functional done

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Suspense } from 'react';
+import './App.css';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
+// import { Redirect } from 'react-router';
+
+const App = ({routes = []}) => {
+  const content =  routes.map((item) => {
+    const Component = React.lazy(() => import(`./${item.path}`));
+      console.log(Component);
+      return (<Route key={`/${item.id}`} path={item.id} element={<Suspense><Component /></Suspense>} />) 
+  });
+
+  const tabs = routes.map((item) => (
+    <li key={item.order}>
+      <Link to={item.id} className="link">
+        {item.title}
+      </Link>
+    </li>
+  ));
+  
+  return (
+  <main>
+    <ul>
+      {tabs}
+    </ul>
+    <Routes>
+      {<Route path='/' element={<Navigate to={routes[0].id} />}/>}
+      {content}
+    </Routes>
+  </main>
+  )
+}
+
+
+export default App;
